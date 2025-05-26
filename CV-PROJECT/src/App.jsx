025d3b7888d1639e61a1df38eec2fb2310c38182
Miguel <aguirremorenomiguelangel2008@gmail.com>
2025-05-26 16:35:31 -0500

    Cambios realizados

---
+++ CV-PROJECT/src/App.jsx
@@ -0,0 +1,17 @@
+import CabeceraCV from "./components/CabeceraCV.jsx";
+import Perfil from "./components/perfil.jsx";
+import Educacion from "./components/educacion.jsx";
+import Experiencia from "./components/experiencia.jsx";
+
+function App() {
+  return (
+    <div>
+      <CabeceraCV />
+      <Perfil />
+      <Educacion />
+      <Experiencia />
+    </div>
+  );
+}
+
+export default App;
