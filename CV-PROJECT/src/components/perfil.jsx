4c7b52172a00bca3c1eb83c6657fb5ecfb94272e
miguelucho425526 <aguirremorenomiguelangel2008@gmail.com>
2025-06-04 19:44:25 +0000

    mi septimo commit

--- CV-PROJECT/src/components/perfil.jsx
+++ CV-PROJECT/src/components/perfil.jsx
@@ -1,29 +1,15 @@
 import "./perfil.css"; 
 import { perfil } from '../data/cvData';
-const perfil = {
-  titulo: "Perfil",
-    descripcion: "Soy un desarrollador web apasionado por crear aplicaciones interactivas y eficientes. Me especializo en el desarrollo frontend utilizando tecnologías modernas como React, JavaScript y CSS. Siempre estoy buscando aprender nuevas habilidades y mejorar mis conocimientos en el campo del desarrollo web.",
-    habilidades: [
-      "HTML",
-      "CSS",
-      "JavaScript",
-      "React",
-      "Node.js"
-    ]
-};
+import React from "react";
 
-export default function Perfil() {
+export default function Perfil({ texto }) {
   return (
     <section>
-      <h3>{perfil.titulo}</h3>
-      <p>{perfil.descripcion}</p>
-      <h4>Habilidades:</h4>
-      <ul>
-        {perfil.habilidades.map((habilidad, index) => (
-          <li key={index}>{habilidad}</li>
-        ))}
-      </ul>
+      <h3>Perfil</h3>
+      <p>{texto}</p>
+      <hr />
     </section>
   );
 }
+
 
